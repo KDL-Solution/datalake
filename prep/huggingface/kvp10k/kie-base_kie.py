aa713f858aece18c8ba87d346084576532e0156e
KimRass <rmx1000@yonsei.ac.kr>
2025-06-13 15:25:16 +0900

    feat: batch 처리, 로드 실패한 .pdf 띄어넘기.

--- prep/huggingface/kvp10k/kie-base_kie.py
+++ prep/huggingface/kvp10k/kie-base_kie.py
@@ -1,40 +1,28 @@
-from datasets import load_dataset
-from io import BytesIO
-from pathlib import Path
-import pdfplumber
 import requests
-from typing import Tuple, List, Dict
-from io import BytesIO
-import pdfplumber
 import json
-from urllib.parse import urlparse
 import json
 import requests
-from pathlib import Path
-import pandas as pd
-from tqdm import tqdm
 import fitz  # PyMuPDF
 import requests
-import pandas as pd
-from tqdm import tqdm
-from pathlib import Path
 import json
+from datasets import load_dataset, Dataset
+from pathlib import Path
+from typing import List, Dict, Any, Set
+from urllib.parse import urlparse
+from pathlib import Path
+from pathlib import Path
 from PIL import Image
 from urllib.parse import urlparse, unquote
-from io import StringIO
-import contextlib
 
+import sys
+sys.path.insert(0, "C:/Users/korea/workspace/datalake/")
 from prep.utils import DATALAKE_DIR, get_safe_image_hash_from_pil
 
 
-# @contextlib.contextmanager
-# def suppress_output():
-#     with contextlib.redirect_stdout(StringIO()), contextlib.redirect_stderr(StringIO()):
-#         yield
-
-
-def build_dynamic_headers(pdf_url: str) -> dict:
-    parsed = urlparse(pdf_url)
+def build_dynamic_headers(
+    url: str,
+) -> dict:
+    parsed = urlparse(url)
     host = parsed.netloc
     referer = f"{parsed.scheme}://{host}"
     return {
@@ -53,177 +41,261 @@ def build_dynamic_headers(pdf_url: str) -> dict:
     }
 
 
-def download_pdf_with_dynamic_headers(
-    pdf_url: str,
-    save_dir: str,
-    chunk_size: int = 8192,
-) -> Path:
-    file_name = Path(unquote(urlparse(pdf_url).path)).name
-    output_path = Path(save_dir) / file_name
-    if output_path.exists():
-        return output_path.as_posix()
-
-    output_path.parent.mkdir(parents=True, exist_ok=True)
-    headers = build_dynamic_headers(pdf_url)
-
-    response = requests.get(pdf_url, headers=headers, stream=True, timeout=10)
-    response.raise_for_status()
-
-    content_type = response.headers.get("Content-Type", "")
-    if "application/pdf" not in content_type:
-        raise ValueError(f"Unexpected content type: {content_type}")
-
-    with open(output_path, "wb") as f:
-        for chunk in response.iter_content(chunk_size=chunk_size):
-            f.write(chunk)
-    return output_path.as_posix()
+class KVP10kProcessor(object):
+    def __init__(
+        self,
+    ):
+        self.failed = set()
 
 
-def extract_text_from_normalized_bbox(page, norm_bbox, dpi=144):
-    width, height = page.rect.width, page.rect.height
-    rect = fitz.Rect(
-        norm_bbox[0] * width,
-        norm_bbox[1] * height,
-        norm_bbox[2] * width,
-        norm_bbox[3] * height,
-    )
-    text = page.get_text("text", clip=rect).strip()
+    def download_pdf(
+        self,
+        pdf_url: str,
+        save_dir: str,
+        chunk_size: int = 8192,
+        timeout: int = 5,
+    ) -> Path:
+        if pdf_url in self.failed:
+            return False
 
-    # render cropped image
-    pix = page.get_pixmap(matrix=fitz.Matrix(dpi / 72, dpi / 72), clip=rect)
-    img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-    return text, img
+        file_name = Path(unquote(urlparse(pdf_url).path)).name
+        output_path = Path(save_dir) / file_name
+        if output_path.exists():
+            return True
 
-
-def process_dataset_to_parquet(
-    dataset,
-    data_dir,
-    images_dir,
-    output_parquet_path,
-    dpi=144,
-    indent=None,
-):
-    images_dir = Path(images_dir).resolve()
-
-    # .pdf를 다운로드부터 받아 놓음:
-    failed = set(),
-    for example in tqdm(dataset, desc="Downloading dataset"):
-        pdf_url = example["image_url"]
+        headers = build_dynamic_headers(
+            pdf_url,
+        )
         try:
-            pdf_path = download_pdf_with_dynamic_headers(
-                pdf_url=pdf_url,
-                save_dir=data_dir,
+            response = requests.get(
+                pdf_url,
+                headers=headers,
+                stream=True,
+                timeout=timeout,
             )
-        except requests.exceptions.HTTPError:
-            if pdf_url not in failed:
-                print(f"Failed to fetch PDF: {pdf_url}")
-                failed.add(pdf_url)
-            continue
-        except Exception as e:
-            continue
+            response.raise_for_status()
+        except (
+            requests.exceptions.HTTPError,
+            requests.exceptions.ReadTimeout,
+        ):
+            self.failed.add(pdf_url)
+            return False
 
-    rows = []
-    for example in tqdm(dataset, desc="Processing dataset"):
-        pdf_url = example["image_url"]
-        try:
-            pdf_path = download_pdf_with_dynamic_headers(
-                pdf_url=pdf_url,
-                save_dir=data_dir,
-            )
-            pdf = fitz.open(pdf_path)
-        except requests.exceptions.HTTPError:
-            continue
-        except Exception as e:
-            print(f"Failed to open PDF: {pdf_url}\n{e}")
-            continue
+        content_type = response.headers.get("Content-Type", "")
+        if "application/pdf" not in content_type:
+            self.failed.add(pdf_url)
+            return False
 
-        try:
-            page = pdf[example["page_number"] - 1]
-            pix = page.get_pixmap(matrix=fitz.Matrix(dpi / 72, dpi / 72))
-            image = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-            width, height = image.size
-        except Exception as e:
-            # print(len(pdf))
-            # print(f"Failed to process page: {e}")
-            continue
+        output_path.parent.mkdir(
+            parents=True,
+            exist_ok=True,
+        )
+        with open(output_path, "wb") as f:
+            for chunk in response.iter_content(chunk_size=chunk_size):
+                f.write(chunk)
+        return True
 
-        data = {}
-        for el in example["annotations"]:
-            linking = el["attributes"].get("Linking")
-            if linking is None:
+    def save_images_and_generate_labels(
+        self,
+        batch: Dict[str, List[Any]],
+        pdfs_dir: str,
+        images_dir: str,
+        dpi: int = 144,
+        indent: int = None,
+    ) -> Dict[str, List[Any]]:
+        images_dir = Path(images_dir).resolve()
+
+        image_paths = []
+        widths = []
+        heights = []
+        labels = []
+        for example in zip(*batch.values()):
+            image_path = width = height = label_str = None
+
+            example = dict(zip(batch.keys(), example))
+
+            file_name = Path(unquote(urlparse(example["image_url"]).path)).name
+            pdf_path = Path(pdfs_dir) / file_name
+            if not pdf_path.exists():
                 continue
 
-            coords = el["coordinates"]
-            points = [(pt['x'], pt['y']) for pt in coords]
-            xs = [pt[0] for pt in points]
-            ys = [pt[1] for pt in points]
-            left, right = min(xs), max(xs)
-            top, bottom = min(ys), max(ys)
+            pdf = fitz.open(pdf_path)
+            try:
+                page = pdf[example["page_number"] - 1]
+            except IndexError:
+                continue
 
-            text, cropped = extract_text_from_normalized_bbox(
-                page=page,
-                norm_bbox=(left, top, right, bottom),
-                dpi=dpi,
+            pix = page.get_pixmap(
+                matrix=fitz.Matrix(
+                    dpi / 72,
+                    dpi / 72,
+                ),
             )
+            image = Image.frombytes(
+                "RGB",
+                [pix.width, pix.height],
+                pix.samples,
+            )
+            width, height = image.size
+            image_hash = get_safe_image_hash_from_pil(
+                image,
+            )
+            image_path = Path(f"{images_dir / image_hash[: 2] / image_hash}.jpg")
+            if not image_path.exists():
+                image_path.parent.mkdir(
+                    parents=True,
+                    exist_ok=True,
+                )
+                image.save(
+                    image_path,
+                    format="JPEG",
+                )
 
-            data[el["_id"]] = {
-                "text": text,
-                "type": el["label"],
-                "key": linking["value"],
-                "bbox": [
-                    round(left * width),
-                    round(top * height),
-                    round(right * width),
-                    round(bottom * height),
-                ],
-            }
+            data = {}
+            for annot in example["annotations"]:
+                linking = annot["attributes"].get("Linking")
+                if linking is None:
+                    continue
 
-        if not data:
-            continue
+                points = [(pt['x'], pt['y']) for pt in annot["coordinates"]]
+                xs = [pt[0] for pt in points]
+                ys = [pt[1] for pt in points]
+                left = min(xs)
+                top = min(ys)
+                right = max(xs)
+                bottom = max(ys)
+                rect = fitz.Rect(
+                    left * page.rect.width,
+                    top * page.rect.height,
+                    right * page.rect.width,
+                    bottom * page.rect.height,
+                )
+                text = page.get_text(
+                    "text",
+                    clip=rect,
+                ).strip()
 
-        label_dict = {}
-        for k, v in data.items():
-            if v["key"] in data:
-                value = data[k]["text"].strip()
-                if value:
-                    label_dict[data[v["key"]]["text"]] = {
-                        "<|value|>": value,
-                        "<|bbox|>": data[k]["bbox"],
-                    }
+                data[annot["_id"]] = {
+                    "text": text,
+                    # "type": annot["label"],
+                    "key": linking["value"],
+                    "bbox": [
+                        round(left * width),
+                        round(top * height),
+                        round(right * width),
+                        round(bottom * height),
+                    ],
+                }
 
-        label_str = json.dumps(
-            label_dict,
-            ensure_ascii=False,
-            indent=indent,
+            if data:
+                label_dict = {}
+                for k, v in data.items():
+                    if v["key"] in data:
+                        value = data[k]["text"].strip()
+                        if value:
+                            label_dict[data[v["key"]]["text"]] = {
+                                "<|value|>": value,
+                                "<|bbox|>": data[k]["bbox"],
+                            }
+
+                if label_dict:
+                    label_str = json.dumps(
+                        label_dict,
+                        ensure_ascii=False,
+                        indent=indent,
+                    )
+            image_paths.append(
+                Path(*image_path.parts[-2:]).as_posix()
+            )
+            widths.append(width)
+            heights.append(height)
+            labels.append(label_str)
+        return {
+            "image_path": image_paths,
+            "width": widths,
+            "height": heights,
+            "label": labels,
+        }
+
+    def export(
+        self,
+        dataset: Dataset,
+        pdfs_dir: str,
+        images_dir: str,
+        parquet_path: str,
+        dpi: int = 144,
+        indent: int = None,
+        batch_size: int = 32,
+    ):
+        # dataset=train_dataset
+        def filter_valid_pdfs(
+            batch: Dict[str, List[Any]],
+                pdfs_dir: str,
+            ) -> List[bool]:
+                keep = []
+                for example in zip(*batch.values()):
+                    example = dict(zip(batch.keys(), example))
+
+                    keep.append(
+                        self.download_pdf(
+                            pdf_url=example["image_url"],
+                            save_dir=pdfs_dir,
+                        )
+                    )
+                return keep
+
+        # .pdf를 다운로드부터 받아 놓음:
+        dataset = dataset.filter(
+            lambda x: filter_valid_pdfs(
+                x,
+                pdfs_dir=pdfs_dir,
+            ),
+            batched=True,
+            batch_size=batch_size,
+            desc="Filtering valid PDFs",
+        )
+        if self.failed:
+            print("\n❌ Failed to download the following PDFs:")
+            for url in self.failed:
+                print(" •", url)
+
+        dataset = dataset.map(
+            lambda x: self.save_images_and_generate_labels(
+                batch=x,
+                pdfs_dir=pdfs_dir,
+                images_dir=images_dir,
+                dpi=dpi,
+                indent=indent,
+            ),
+            batched=True,
+            batch_size=batch_size,
+            remove_columns=dataset.column_names,
+        )
+        dataset = dataset.filter(
+            lambda x: x["label"] is not None,
+            desc="Filtering valid samples",
         )
 
-        image_hash = get_safe_image_hash_from_pil(cropped)
-        image_path = images_dir / image_hash[:2] / f"{image_hash}.jpg"
-        image_path.parent.mkdir(parents=True, exist_ok=True)
-        if not image_path.exists():
-            image.save(image_path, format="JPEG")
-
-        rows.append({
-            "image_path": Path(*image_path.parts[-2:]).as_posix(),
-            "width": width,
-            "height": height,
-            "label": label_str,
-        })
-
-    df = pd.DataFrame(rows)
-    df.to_parquet(output_parquet_path, index=False)
-    return df
+        parquet_path = Path(parquet_path).resolve()
+        parquet_path.parent.mkdir(
+            parents=True,
+            exist_ok=True,
+        )
+        dataset.to_pandas().to_parquet(
+            parquet_path.as_posix(),
+            index=False,
+        )
 
 
 if __name__ == "__main__":
     datalake_dir = "W:/datalake"
     dataset="kvp10k"
-    data_dir = Path(datalake_dir) / f"source/provider=huggingface/dataset={dataset}"
+    pdfs_dir = Path(datalake_dir) / f"source/provider=huggingface/dataset={dataset}"
     train_dataset, test_dataset = load_dataset(
         "parquet",
         data_files={
-            "train": (data_dir / "train/*.parquet").as_posix(),
-            "test": (data_dir / "test/*.parquet").as_posix(),
+            "train": (pdfs_dir / "train/*.parquet").as_posix(),
+            "test": (pdfs_dir / "test/*.parquet").as_posix(),
         },
         split=[
             "train",
@@ -231,18 +303,16 @@ if __name__ == "__main__":
         ],
     )
 
-    process_dataset_to_parquet(
+    processor = KVP10kProcessor()
+    processor.export(
         dataset=train_dataset,
-        data_dir="C:/Users/korea/workspace/datalake/prep/huggingface/kvp10k/data",
+        pdfs_dir="C:/Users/korea/workspace/datalake/prep/huggingface/kvp10k/pdfs",
         images_dir="C:/Users/korea/workspace/datalake/prep/huggingface/kvp10k/images_train",
-        output_parquet_path="C:/Users/korea/workspace/datalake/prep/huggingface/kvp10k/train.parquet",
+        parquet_path="C:/Users/korea/workspace/datalake/prep/huggingface/kvp10k/train.parquet",
     )
-    process_dataset_to_parquet(
+    export(
         dataset=test_dataset,
-        data_dir="C:/Users/korea/workspace/datalake/prep/huggingface/kvp10k/data",
+        pdfs_dir="C:/Users/korea/workspace/datalake/prep/huggingface/kvp10k/pdfs",
         images_dir="C:/Users/korea/workspace/datalake/prep/huggingface/kvp10k/images_test",
-        output_parquet_path="C:/Users/korea/workspace/datalake/prep/huggingface/kvp10k/test.parquet",
+        parquet_path="C:/Users/korea/workspace/datalake/prep/huggingface/kvp10k/test.parquet",
     )
-
-    # example = test_dataset[0]
-    # pdf_url = example["image_url"]
